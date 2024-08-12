ee96cc0c333e8dc14c6dd6a60a47d4efede89d76
gabean13 <gazette9@naver.com>
2024-08-12 14:50:47 +0900

    feat : responseEntity<response dto>로 통일

---
+++ src/main/java/org/c4marathon/assignment/dto/ResponseDto.java
@@ -0,0 +1,10 @@
+package org.c4marathon.assignment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ResponseDto {
+    private String message;
+}
