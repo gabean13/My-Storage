2c981f9a779a78b6702d1b76e1f902287b0e561f
gabean13 <gazette9@naver.com>
2024-08-12 14:50:07 +0900

    feat : file separator 추가, log 삭제, 에러 메세지 수정

--- src/main/java/org/c4marathon/assignment/service/FileUploadService.java
+++ src/main/java/org/c4marathon/assignment/service/FileUploadService.java
@@ -9,7 +9,8 @@ import org.c4marathon.assignment.data.entity.User;
 import org.c4marathon.assignment.data.repository.FileRepository;
 import org.c4marathon.assignment.data.repository.MetadataRepository;
 import org.c4marathon.assignment.data.repository.UserRepository;
-import org.c4marathon.assignment.exception.FileEmptyException;
+import org.c4marathon.assignment.dto.ResponseDto;
+import org.c4marathon.assignment.exception.FileExtensionInvalidException;
 import org.c4marathon.assignment.exception.UserNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -29,18 +30,29 @@ public class FileUploadService {
     private final UserRepository userRepository;
     private final FileRepository fileRepository;
     private final MetadataRepository metadataRepository;
+    private final ContentTypeToExtensionMapper contentTypeToExtensionMapper;
 
-    @Transactional
-    public void uploadFileHandler (MultipartFile file, String userKey) throws IOException {
-        if(file == null || file.isEmpty()){
-            throw new FileEmptyException("파일이 비어 있습니다.");
+    public ResponseDto uploadFileListHandler (MultipartFile[] fileList, String userKey) throws IOException {
+        User user = userRepository.findUserByKey(userKey).orElseThrow(() -> new UserNotFoundException());
+
+        for(MultipartFile file : fileList){
+           uploadFileHandler(file, user);
         }
 
-        User user = userRepository.findUserByKey(userKey).orElseThrow(() -> new UserNotFoundException("유저가 존재하지 않습니다"));
+        return new ResponseDto(fileList.length + "개 파일 업로드를 성공하였습니다");
+    }
+
+    @Transactional
+    protected String uploadFileHandler(MultipartFile file, User user) throws IOException{
+        if(!contentTypeToExtensionMapper.isContainsContentType(file.getContentType())){
+            throw new FileExtensionInvalidException();
+        }
 
         String fileUUID = saveFileToStorage(user.getKey(), file);
         Long fileId = saveFileInfo(file.getOriginalFilename(), user, fileUUID);
         saveMetadataInfo(fileId, file);
+
+        return file.getOriginalFilename();
     }
 
     /**
@@ -49,7 +61,7 @@ public class FileUploadService {
     protected String saveFileToStorage(String userUUID, MultipartFile file) throws IOException {
         String uniqueFileName = UUID.randomUUID().toString();
 
-        String userPath = "storage" + "\\" + userUUID;
+        String userPath = "storage" + java.io.File.separator + userUUID;
 
         Path uploadPath = Path.of(userPath);
         Path filePath = uploadPath.resolve(uniqueFileName);
@@ -64,11 +76,13 @@ public class FileUploadService {
     }
 
     protected Long saveFileInfo(String fileName, User user, String fileUUID){
-        String path = "storage" + "\\" + user.getKey();
+        String path = "storage" + java.io.File.separator + user.getKey();
 
-        //TODO : 같은 이름의 파일 저장시 이름 저장 방법
-        //List<File> sameNameFileList = fileRepository.findAllByUserIdAndName(userId, fileName);
-        File file = new File(user.getId(), path, fileName, fileUUID);
+        File file = File.builder()
+                .name(fileName)
+                .path(path)
+                .userId(user.getId())
+                .uuid(fileUUID).build();
         File savedFile = fileRepository.save(file);
 
         return savedFile.getId();
